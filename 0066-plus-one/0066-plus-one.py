87b254dcdec1b285ae393db4c849bf1e7e5d7502
Saurabh Ranjan <91522984+saurabhranjan11@users.noreply.github.com>
2026-01-01 10:44:46 +0530

    Time: 0 ms (100%), Space: 17 MB (99.96%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,5 +1,11 @@
+#another method by conversion of list to integer and integer to list.
+#less complexity than array manipulation.
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        new = int("".join(map(str,(digits))))
-        new += 1 
-        return list(map(int,str(new)))
+        n = len(digits)
+        for i in range(n-1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        return [1] + digits
